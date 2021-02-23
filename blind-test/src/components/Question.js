60991c600a8c9b46bd5b5d06d8f2c23c11e41399
manonfrgs <manon.fargues@ynov.Com>
2021-02-23 11:54:28 +0100

    :art:

--- blind-test/src/components/Question.js
+++ blind-test/src/components/Question.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Button } from '@material-ui/core';
 
+import "../styles/Home.css"
 
 function Question() {
     return (
         <>
-        <h1>QUESTION</h1>
-        <div>
-            <h2>Question : </h2>
-            <p>Quel artiste interprète cette chanson?</p>
-        </div>
-        <div>
-            <h2>Réponses : </h2>
+        <div class="center">
             <div>
-                <Button variantt="contained" color="primary">Réponse 1</Button>
-                <Button variantt="contained" color="primary">Réponse 2</Button>
+                <h2>Question : </h2>
+                <p>Quel artiste interprète cette chanson?</p>
             </div>
             <div>
-                <Button variantt="contained" color="primary">Réponse 3</Button>
-                <Button variantt="contained" color="primary">Réponse 4</Button>
+                <h2>Réponses : </h2>
+                <div>
+                    <Button variant="contained" color="primary">Réponse 1</Button>
+                    <Button variant="contained" color="primary">Réponse 2</Button>
+                </div>
+                <div>
+                    <Button variant="contained" color="primary">Réponse 3</Button>
+                    <Button variant="contained" color="primary">Réponse 4</Button>
+                </div>
             </div>
         </div>
         </>
