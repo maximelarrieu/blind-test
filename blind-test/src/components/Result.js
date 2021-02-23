80138fe4fa2c4d5232eb6266cbccc76b77e07ab3
manonfrgs <manon.fargues@ynov.Com>
2021-02-23 11:31:36 +0100

    :sparkles: new Result page

---
+++ blind-test/src/components/Result.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '@material-ui/core';
+
+
+function Result() {
+    return (
+        <>
+        <h1>RESULT</h1>
+        <p>Votre score est : score</p>
+        <p>Merci d'avoir joué!</p>
+        <Button variantt="contained" color="primary">REJOUER</Button>
+        </>
+    )
+}
+
+export default Result
