1f6102f7a5bef36208e9d3ad38cb577cd87c32f0
manonfrgs <manon.fargues@ynov.Com>
2021-02-23 11:54:11 +0100

    :art:

--- blind-test/src/components/Game.js
+++ blind-test/src/components/Game.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Button } from '@material-ui/core';
 
+import "../styles/Home.css"
+
 function Game() {
     return (
-        <Button variant="contained" color="primary">
-            <a href="/question">New game</a>
-        </Button>
+        <>
+        <div class="center">
+            <h2>Nouvelle partie</h2>
+            <Button variant="contained" color="primary" >
+                <a href="/question">GO !</a>
+            </Button>
+        </div>
+        </>
     )
 }
 
