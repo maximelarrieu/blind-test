3ac7278b8bbd548c71318f94058bcc757a5d4e66
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-01 17:47:53 +0100

    :sparkles: add provider

--- blind-test/src/App.js
+++ blind-test/src/App.js
@@ -1,28 +1,18 @@
 import './App.css';
+import { Provider, useSelector } from 'react-redux'
+import { store } from './store'
 
 import { BrowserRouter as Router, Route, Switch, PrivateRoute} from 'react-router-dom'
 
-import NavBar from './components/NavBar'
-import Home from './components/Home'
-import Game from './components/Game'
-import Question from './components/Question'
-import Profile from './components/Profile'
-import Result from './components/Result'
-import Authenticate from './components/Authenticate'
+import Navigation from './components/Navigation'
 
 function App() {
+  // const player = useSelector((state) => state.user.player)
+  // console.log(player)
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/login" component={Authenticate} />
-        <Route path="/game" component={Game} />
-        <Route path="/question" component={Question} />
-        <Route path="/result" component={Result} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
   );
 }
 
