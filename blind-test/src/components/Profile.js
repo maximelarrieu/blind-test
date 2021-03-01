ce0fb77dbc77ae3344d1b8dd1f59fa17aa1e1b92
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-01 17:49:35 +0100

    :zap: in new page, user is disconnected, can't go in profile

--- blind-test/src/components/Profile.js
+++ blind-test/src/components/Profile.js
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import { useEffect, useState } from "react";
 import {Button} from '@material-ui/core'
 import '../styles/Home.css'
+import { useSelector } from 'react-redux'
 
 function Profile() {
+    const player = useSelector((state) => state.user)
+    console.log(player)
+    // const [isAdmin, setIsAdmin] = useState(data.backOffice);
     return (
         <div className="center">
             <img src="https://icotar.com/initials/user" alt="avatar" style={{width: 50}}/>
-            <h2>Hello --user--</h2>
+            {/* <h2>Hello {props.name}</h2> */}
+            <h2>Hello {player.name}</h2>
             <Button variant="contained" color="primary">
                 <a href="/logout">Sign Out</a>
             </Button>
